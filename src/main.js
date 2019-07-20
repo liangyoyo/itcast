b0d9d3d06672c3688ba889bd8ec148dc690fd26a
liangyoyo <liangjyoyo@126.com>
2019-07-20 12:30:34 +0800

    表单的基本结构完成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入 element-ui
+import ElementUI from 'element-ui'
+// 引入 element-ui 样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 引入公共样式
+import '@/styles/index.less'
+// 引入路由文件
 import router from '@/router/router_index.js'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  // 注入
   router,
   render: h => h(App)
 }).$mount('#app')
